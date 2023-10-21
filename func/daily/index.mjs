6dd37b5b734b9fc367706e7eb28e658918c86325
Taras Kontsur <kontsur@gmail.com>
2023-10-22 01:04:38 +0300

    Added daily reminder lambda function

---
+++ func/daily/index.mjs
@@ -0,0 +1,37 @@
+import dayjs from 'dayjs';
+import AWS from 'aws-sdk';
+import TelegramBot from 'node-telegram-bot-api';
+
+export const handler = async (event) => {
+    AWS.config.update({ region: 'eu-central-1' });
+    // load moving.json from S3 bucket with ARN arn:aws:s3:::calendars-hosting1 using AWS S3 SDK
+    const file  = await new AWS.S3().getObject({ Bucket: 'calendars-hosting1', Key: 'moving.json' }).promise();
+    const holidays = JSON.parse(file.Body.toString());
+    const hasMovingHoliday = (date) => !!holidays[date];
+      
+    const normalizeDate = (date) => date.add(13, 'd').format('MM-DD');
+
+    const getMovigHoliday = (date) => holidays[date];  
+    const tomorrow = normalizeDate(dayjs().add(1, 'd'));
+    console.log(`Normalized tomorrow is: ${tomorrow}`);
+    
+    if (hasMovingHoliday(tomorrow)) {
+      const holiday = getMovigHoliday(tomorrow);
+      console.log(`Today is holiday: ${holiday}`);
+      try {
+        const telegramBot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: false });
+        const answer = await telegramBot.sendMessage(process.env.TELEGRAM_CHAT_ID, `Нагадую, завтра свято: *${holiday}*!`, { parse_mode: 'Markdown' });
+        console.log(`Sent the message successfully: ${answer}`);
+      } catch (e) {
+        console.log('Error sending message', e);
+      }
+    } else {
+      console.log('No holidays today');
+    }
+    
+    return {
+        statusCode: 200,
+        body: ''
+    };
+  };
+  
